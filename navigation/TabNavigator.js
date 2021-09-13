2c368509059db1c06dc8ab44acb2f59770dc15a6
Areen-Rath <70901420+Areen-Rath@users.noreply.github.com>
2021-09-13 14:44:56 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from '../screens/Feed';
+import CreatePost from '../screens/CreatePost';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            labeled={false}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if(route.name === "Feed"){
+                        iconName = focused ? "book" : "book-outline"
+                    } else if(route.name === "Create Post"){
+                        iconName = focused ? "create": "create-outline"
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />
+                }
+            })}
+            tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "grey"
+            }}>
+                <Tab.Screen name="Feed" component={Feed} />
+                <Tab.Screen name="Create Post" component={CreatePost} />
+        </Tab.Navigator>
+    );
+}
+
+export default BottomTabNavigator;
---
+++ screens/Feed.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class Feed extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Feed</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
